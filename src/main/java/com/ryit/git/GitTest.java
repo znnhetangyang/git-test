bad0d02b0a734f9ecc5e1c912d2f74244fbcb2de
tangyang <t13874786141@163.com>
2022-05-26 19:42:44 +0800

    second commit

--- src/main/java/com/ryit/git/GitTest.java
+++ src/main/java/com/ryit/git/GitTest.java
@@ -3,7 +3,11 @@ package com.ryit.git;
 public class GitTest {
 
     public static void main(String[] args) {
+
         System.out.println("hello git");
+        System.out.println("hello git  22222");
+
     }
+
 
 }
